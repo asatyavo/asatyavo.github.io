aed829889d1e9a01b3a037c380e5b0c8e8e8a2fd
asatyavo <71343750+asatyavo@users.noreply.github.com>
2021-10-09 13:56:03 -0400

    Update calculator.js

--- itis3135/scripts/calculator.js
+++ itis3135/scripts/calculator.js
@@ -96,45 +96,13 @@ keys.addEventListener('click', e=>{
         }
         calculator.dataset.previousKeyType = type
     }
-})
-/*
-function calculate(e){
-    const calculator = document.querySelector(".calculator")
-    const keys = calculator.querySelector(".calculator__keys")
     
-    
-
-    keys.addEventListener('click',e=>{
-        if(e.target.matches('button')){
-            // do something
-        }
-    })
-    
-    const key = e.target
-    const action = key.dataset.action
-    if(!action){
-        console.log('number key!')
-    }
-    
-    if(
-        action === 'add'||
-        action === 'subtract'||
-        action === 'multiply'||
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
         action === 'divide'
-    ){
-        console.log('operator key!')
-    }
-    if(action === 'decimal'){
-        console.log('decimal key!')
-    }
-    if(action === 'clear'){
-        console.log('clear key!')
-    }
-    if(action=== 'calculate'){
-        console.log("equal key")
-    }
-     
-
- 
-}
-*/
+      ) {
+        key.classList.add('is-depressed')
+      }
+})
