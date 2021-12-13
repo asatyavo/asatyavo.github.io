cf6611f7678a9e6067603e20c497006b87958a65
asatyavo <71343750+asatyavo@users.noreply.github.com>
2021-12-12 20:50:16 -0500

    Create index.js

---
+++ itis3135/project/scripts/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        slideWidth: 300,
+        slideMargin: 10,
+        pause: 3000,
+        adaptiveHeight: true,
+       // pagerType:'short',
+      // pagerSelector:'#id_pager',
+
+    });
+});
